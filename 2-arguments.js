81e8d2773854e47c8bf66d39508dd8d6debdbe1a
KinggPeculiar <KingsleyPeculiar04@gmail.com>
2025-07-11 12:03:16 +0100

    Added new file

---
+++ 2-arguments.js
@@ -0,0 +1,14 @@
+// Write a script that prints a message depending of the number of arguments passed:
+
+//     If no arguments are passed to the script, print “No argument”
+//     If only one argument is passed to the script, print “Argument found”
+//     Otherwise, print “Arguments found”
+//     You must use console.log(...) to print all output
+//     You are not allowed to use var
+function sum(num1, num2){
+    return num1 + num2;
+}
+
+let result = sum(5, 2);
+
+console.log(result);
