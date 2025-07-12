2b37b1e048c16d6a91e99218184d5f3ae1f34740
KinggPeculiar <KingsleyPeculiar04@gmail.com>
2025-07-12 12:01:00 +0100

    Updated new fix to argument problem

--- 2-arguments.js
+++ 2-arguments.js
@@ -5,6 +5,19 @@
 //     Otherwise, print “Arguments found”
 //     You must use console.log(...) to print all output
 //     You are not allowed to use var
+
+// let sum1 = 1;
+// let sum2 = 1;
+
+// const co = [sum1, sum2];
+
+// for(let co = 0; co <= 1; )
+// if (co.lenght === 0) {
+//     console.log('1 argument');
+// } else if (code.lenght === 1) {
+//     console.log('0 argument');
+// }
+
 const code = ['java', 'javascript', 'python'];
 
 if (code.length === 0) {
@@ -15,13 +28,22 @@ if (code.length === 0) {
   console.log('Arguments found');
 }
 
-let sum1 = 3;
-let sum2 = 7;
+const code = ['java', 'python'];
 
-const co = [sum1, sum2];
+if (code.length === 0) {
+  console.log('No argument');
+} else if (code.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
 
-if (co.lenght === 0) {
-    console.log('1 argument');
-} else if (code.lenght === 1) {
-    console.log('0 argument');
+const code = ['java'];
+
+if (code.length === 0) {
+  console.log('No argument');
+} else if (code.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
